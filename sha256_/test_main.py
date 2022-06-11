beb6d0cb575bd1b31f0dd77aaf2dfd37c5b45428
GivingWestwood <106045019+GivingWestwood@users.noreply.github.com>
2022-06-11 22:52:45 +0800

    Update test_main.py

--- sha256_/test_main.py
+++ sha256_/test_main.py
@@ -7,7 +7,7 @@ from main import compute_sha256
 
 class Test(TestCase):
 	def test_compute_sha256(self):
-		sha256_swufe='090b235e9eb8f197f2dd927937222c570396d971222d9009a9189e2b6cc0a2c1'
+		sha256_swufe='eb6fd8ddbc39ec5437a4363f6c0fcdfa183254f5d6c2b54378b19dae8437e4fd'
 		self.assertEqual(compute_sha256('swufe'), sha256_swufe,'error,not equal')
 
 
