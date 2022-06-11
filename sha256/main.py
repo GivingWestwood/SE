553b5bf4eaa2cf6f9b93b42bb05a0bcd4ec7bdea
GivingWestwood <106045019+GivingWestwood@users.noreply.github.com>
2022-06-11 22:53:37 +0800

    Delete sha256 directory

--- sha256/main.py
+++
@@ -1,7 +0,0 @@
-#import hashlib
-#actions里安装hashlib库
-
-def compute_sha256(input):
-    #h = hashlib.new('sha256',b"swufe")
-    #return h.hexdigest()
-    return '090b235e9eb8f197f2dd927937222c570396d971222d9009a9189e2b6cc0a2c1'
