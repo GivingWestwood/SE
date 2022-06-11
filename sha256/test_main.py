447bbbc60be1ba70fe72061afc3849f6a9067687
GivingWestwood <106045019+GivingWestwood@users.noreply.github.com>
2022-06-11 22:42:50 +0800

    Update test_main.py

--- sha256/test_main.py
+++ sha256/test_main.py
@@ -9,6 +9,5 @@ class Test(TestCase):
     def test_compute_sha256(self):
     	sha256_swufe='090b235e9eb8f197f2dd927937222c570396d971222d9009a9189e2b6cc0a2c1'
         self.assertEqual(compute_sha256('swufe'), sha256_swufe,'not equal')
-        
 if __name__ == '__main__':
     unittest.main()
